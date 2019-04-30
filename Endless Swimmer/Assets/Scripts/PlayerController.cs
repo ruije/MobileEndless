ade1c006056db61eb4672a5c9bdee6f17b27078f
Jessy Ruiter <jessyruiter@gmail.com>
2019-04-30 21:54:57 +1200

    Enemies
    They now move and do damage.
    Also fixed a bug where the player lurched forward at the start, caused by the moveTowards statement.

--- Endless Swimmer/Assets/Scripts/PlayerController.cs
+++ Endless Swimmer/Assets/Scripts/PlayerController.cs
@@ -12,14 +12,19 @@ public class PlayerController : MonoBehaviour {
     public float maxHeight;
     public float minHeight;
 
+    public int health = 3;
+
+    private bool press = false;
+
    
     void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
+        if (press) {
+            transform.position = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
+        }
         
             buttonUp.onClick.RemoveAllListeners();
             buttonUp.onClick.AddListener(() => PlayerUp());
@@ -38,6 +43,7 @@ public class PlayerController : MonoBehaviour {
         if (transform.position.y < maxHeight)
         {
             newPos = new Vector2(transform.position.x, transform.position.y + Yvalue);
+            press = true;
         }
         
     }
@@ -47,6 +53,7 @@ public class PlayerController : MonoBehaviour {
         if (transform.position.y > minHeight)
         {
             newPos = new Vector2(transform.position.x, transform.position.y - Yvalue);
+            press = true;
         }
     }
 }
