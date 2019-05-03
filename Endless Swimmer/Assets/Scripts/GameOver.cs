8af76c44cfe2efce58352b0abcd9e3a2540f59cb
Jessy Ruiter <jessyruiter@gmail.com>
2019-05-03 20:59:30 +1200

    First Build
    Added a title screen
    Removed some of the sequences of hazards because they seemed unfair
    Built the game into a playable apk for testing
    Adjusted some of the UI elements

---
+++ Endless Swimmer/Assets/Scripts/GameOver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+    public Button curButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(curButton.tag == "Finish")
+        {
+            curButton.onClick.RemoveAllListeners();
+            curButton.onClick.AddListener(() => GameExit());
+        }
+        else if(curButton.tag == "Respawn")
+        {
+            curButton.onClick.RemoveAllListeners();
+            curButton.onClick.AddListener(() => GameContinue());
+        }
+		
+	}
+
+    void GameExit()
+    {
+        Application.Quit();
+        Debug.Log("Application Quits");
+    }
+
+    void GameContinue()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
